d57779da862ad440d78bae8ef69bfeb472fdc2b3
kirill <kirill.dovgal96@mail.ru>
2022-10-19 11:55:49 +0300

    answ_souvenirs

---
+++ 01_basics/souvenirs/answ_souvenirs.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+sov = int(input('Введите количество сувениров: '))
+bezdel = int(input('Введите количество безделушек: '))
+ves = sov * 75 + bezdel * 112
+print(f'Общий вес посылки равен {ves} грамм')
