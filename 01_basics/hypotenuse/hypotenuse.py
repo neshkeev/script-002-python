d1a7fca134accf9d904a45f2a21002eee9a92436
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-19 18:30:04 +0300

    hypotenuse

---
+++ 01_basics/hypotenuse/hypotenuse.py
@@ -0,0 +1,4 @@
+import math
+line_1 = float(input("Введите длину первого катета: "))
+line_2 = float(input("Введите длину второго катета: "))
+print(f"Длина гипотенузы {round((math.sqrt(line_1) + math.sqrt(line_2)),2)}")
