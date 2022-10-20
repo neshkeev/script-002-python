ebc58a4055abef8f9fe1c04f2b16f4b66385ee4b
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-20 09:27:55 +0300

    figures

---
+++ 01_basics/figures/figures.py
@@ -0,0 +1,15 @@
+finder = int(input("введите число : "))
+i = 0
+dot = '*'
+if finder == 4:
+    while i < 4:
+        print(dot*finder) 
+        i += 1
+elif finder == 3:
+    print(f"{dot*finder}\n{dot} {dot}\n{dot*finder}")
+elif finder == 5:
+    finder = 4
+    print(f"{dot*finder}\n{dot}  {dot}\n{dot}  {dot}\n{dot*finder}")
+elif finder == 6:
+    finder = 5
+    print(f"{dot*finder}\n{dot}   {dot}\n{dot}   {dot}\n{dot}   {dot}\n{dot*finder}")
